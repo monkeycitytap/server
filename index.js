08151d6db11657b30f990d96533456d582702676
monkeycitytap <sivayuda54@gmail.com>
2024-10-11 23:35:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Telegraf, Markup } = require("telegraf");
+const TOKEN = "7797629643:AAEWHOonlUro9gfOHAM2F_TODPJQCWonzSw";
+const bot = new Telegraf(TOKEN);
+const express = require("express");
+const app = express()
+app.use(express.json())
+const web_link = "https://monkeycity.netlify.app/";
+const community_link = "https://t.me/Forextappbot";
+
+
+bot.start((ctx) => {
+    const startPayload = ctx.startPayload;
+    const urlSent = `${web_link}?ref=${startPayload}`;
+    const user = ctx.message.from;
+    const userName = user.username ? `@${user.username}` : user.first_name;
+    ctx.replyWithMarkdown(`*Hey, ${userName}! I am @Forextappbot  , Welcome to CDPTap!*
+Mine CDPTap cryptocurrency easily and earn CDPTap tokens.
+
+Start mining now and be among the biggest players earning CDPtap tokens daily.
+
+Got friends, relatives, co-workers?
+Bring them all into the game.
+More squad power, more CDPtap tokens.`, {
+        reply_markup: {
+            inline_keyboard: [
+              [{ text: "👋 Start now!", web_app: { url: urlSent } }],
+              [{ text: "Crypto ", url: community_link }]
+            
+            ],
+            in: true
+        },
+    });
+  });
+
+  
+  bot.launch();
+  
+app.listen(3000, () => {
+    console.log("server is me and now running")
+})
